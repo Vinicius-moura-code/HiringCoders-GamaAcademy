d599974e0af67af9926a5fe47b16765edea962f0
Mr.Vinicius <viniciusrebelo487@gmail.com>
2020-07-07 17:53:16 -0300

    insert pagination

--- Desafio Loja Online - Pokestore/pokestore/src/pages/main/index.js
+++ Desafio Loja Online - Pokestore/pokestore/src/pages/main/index.js
@@ -1,20 +1,63 @@
-import React, {Component} from 'react';
-import api from '../../services/api';
-
+import React, { Component } from "react";
+import api from "../../services/api";
+import "./styles.css";
 
 export default class Main extends Component {
+  state = {
+    pokemons: [],
+    pokeInfo: {},
+    page: 0,
+  };
 
   componentDidMount() {
     this.loadPoke();
   }
 
-  loadPoke = async () => {
-    const response = await api.get('/?offset=410&limit=21');
+  loadPoke = async (page = 0) => {
+    const response = await api.get(`/v2/pokemon?offset=${page}&limit=20`);
 
-    console.log(response.data.results);
-  }
+    const { results, ...pokeInfo } = response.data;
 
-  render(){
-    return <h1>srrs</h1>
+    this.setState({ pokemons: results, pokeInfo, page });
+  };
+
+  prevPage = () => {
+    const { page, pokeInfo } = this.state;
+
+    if (page === 0) return;
+
+    const pageNumber = page - 20;
+
+    this.loadPoke(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, pokeInfo } = this.state;
+
+    if (page === pokeInfo.pages) return;
+
+    const pageNumber = page + 20;
+
+    this.loadPoke(pageNumber);
+  };
+
+  render() {
+    const { pokemons, page, pokeInfo } = this.state;
+
+    return (
+      <div className="pokemon-list">
+        {pokemons.map((pokemon) => (
+          <article key={pokemon._id}>
+            <p>{pokemon.name}</p>
+            <p>{pokemon.url}</p>
+            <a href="#">Buy</a>
+          </article>
+        ))}
+        <div className="actions">
+          <button disabled={ page === 0} onClick={this.prevPage}>Previous</button>
+          <button disabled={ page === pokeInfo.pages} onClick={this.nextPage}>Next</button>
+        </div>
+      </div>
+    );
   }
 }
